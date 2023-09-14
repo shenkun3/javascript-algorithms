34bc2a8e36e723d8dff01d67653f5299e1a80544
shenkun <shenkun@hotmail.com>
2023-09-14 14:24:28 +0800

    docs(comment): 修改一些注释错误

--- src/data-structures/tree/segment-tree/SegmentTree.js
+++ src/data-structures/tree/segment-tree/SegmentTree.js
@@ -31,7 +31,7 @@ export default class SegmentTree {
     } else {
       // If original array length is not a power of two then we need to find
       // next number that is a power of two and use it to calculate
-      // tree array size. This is happens because we need to fill empty children
+      // tree array size. This happens because we need to fill empty children
       // in perfect binary tree with nulls.And those nulls need extra space.
       const currentPower = Math.floor(Math.log2(inputArrayLength));
       const nextPower = currentPower + 1;
